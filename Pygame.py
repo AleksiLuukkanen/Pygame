279f2752f718f491a93975783cc9375e8ebfef0d
AleksiLuukkanen <aleksi.luukkanen@edu.salpaus.fi>
2023-10-05 07:06:03 +0000

    pygame

---
+++ Pygame.py
@@ -0,0 +1,16 @@
+import pygame
+
+naytto = pygame.display.set_mode((640, 400))
+pygame.display.set_caption("Piirtäminen")
+
+def main():
+    while True:
+        tapahtuma = pygame.event.poll()
+        if tapahtuma.type == pygame.QUIT:
+            break
+
+        naytto.fill((0, 0, 0))
+        pygame.draw.line(naytto, (0, 0, 255), (100, 50), (400, 370))
+        pygame.display.flip()
+
+main()
