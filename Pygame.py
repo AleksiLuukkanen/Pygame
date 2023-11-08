027d8c1ddcfbd729fd1d217ba8625f61724e2fd7
AleksiLuukkanen <aleksi.luukkanen@edu.salpaus.fi>
2023-11-08 07:25:59 +0000

    tallennus

--- Pygame.py
+++ Pygame.py
@@ -1,23 +1,44 @@
-import keyboard
 import pygame
+import time
+import sys
 
-naytto = pygame.display.set_mode((640, 400))
-pygame.display.set_caption("Minun Python Pelini.")
-print("Testi")
+pygame.init()
+
+naytto = pygame.display.set_mode((1000, 600))
+pygame.display.set_caption("Vihollis peli.")
+
 x = 20
-y = 20
+y = 60
 
+x2 = 20
+y2 = 60
 def main():
+    global x, y
+
     while True:
         tapahtuma = pygame.event.poll()
         if tapahtuma.type == pygame.QUIT:
-            break
+            pygame.quit()
+            sys.exit()
 
-        naytto.fill((0, 0, 0))
-        hahmo = pygame.image.load("pelihahmo.png").convert()
+        
+        background = pygame.image.load("Maailma.png").convert()
+        naytto.blit(background, (0, 0))
+
+        hahmo = pygame.image.load("vihollinen.png").convert_alpha()
+        hahmo.set_colorkey((0, 0, 0))
+        naytto.blit(hahmo, (x2, y2))
+
+        hahmo = pygame.image.load("pelihahmo.png").convert_alpha()
+        hahmo.set_colorkey((0, 0, 0))
         naytto.blit(hahmo, (x, y))
-        pygame.display.flip()
-        if keyboard.is_pressed("W"):
-            x = x + 5
 
+        pygame.display.flip()
+
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            y = y - 1.5
+        elif pygame.key.get_pressed()[pygame.K_d]:
+            x = x + 1.5
+        elif pygame.key.get_pressed()[pygame.K_a]:
+            x = x - 1.5
 main()
